7f19911dd25119ed567cc130374f564cd992c4e8
Ben Johnson <ben@learncodebygaming.com>
2020-05-17 12:06:46 -0500

    Code for video 3

---
+++ 003_group_rectangles/main.py
@@ -0,0 +1,85 @@
+import cv2 as cv
+import numpy as np
+import os
+
+
+# Change the working directory to the folder this script is in.
+# Doing this because I'll be putting the files from each video in their own folder on GitHub
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def findClickPositions(needle_img_path, haystack_img_path, threshold=0.5, debug_mode=None):
+        
+    # https://docs.opencv.org/4.2.0/d4/da8/group__imgcodecs.html
+    haystack_img = cv.imread(haystack_img_path, cv.IMREAD_UNCHANGED)
+    needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+    # Save the dimensions of the needle image
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
+
+    # There are 6 methods to choose from:
+    # TM_CCOEFF, TM_CCOEFF_NORMED, TM_CCORR, TM_CCORR_NORMED, TM_SQDIFF, TM_SQDIFF_NORMED
+    method = cv.TM_CCOEFF_NORMED
+    result = cv.matchTemplate(haystack_img, needle_img, method)
+
+    # Get the all the positions from the match result that exceed our threshold
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+    #print(locations)
+
+    # You'll notice a lot of overlapping rectangles get drawn. We can eliminate those redundant
+    # locations by using groupRectangles().
+    # First we need to create the list of [x, y, w, h] rectangles
+    rectangles = []
+    for loc in locations:
+        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
+        # Add every box to the list twice in order to retain single (non-overlapping) boxes
+        rectangles.append(rect)
+        rectangles.append(rect)
+    # Apply group rectangles
+    rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
+    #print(rectangles)
+
+    points = []
+    if len(rectangles):
+        #print('Found needle.')
+
+        line_color = (0, 255, 0)
+        line_type = cv.LINE_4
+        marker_color = (255, 0, 255)
+        marker_type = cv.MARKER_CROSS
+
+        # Loop over all the rectangles
+        for (x, y, w, h) in rectangles:
+
+            # Determine the center position
+            center_x = x + int(w/2)
+            center_y = y + int(h/2)
+            # Save the points
+            points.append((center_x, center_y))
+
+            if debug_mode == 'rectangles':
+                # Determine the box position
+                top_left = (x, y)
+                bottom_right = (x + w, y + h)
+                # Draw the box
+                cv.rectangle(haystack_img, top_left, bottom_right, color=line_color, 
+                             lineType=line_type, thickness=2)
+            elif debug_mode == 'points':
+                # Draw the center point
+                cv.drawMarker(haystack_img, (center_x, center_y), marker_color, marker_type)
+
+        if debug_mode:
+            cv.imshow('Matches', haystack_img)
+            cv.waitKey()
+            #cv.imwrite('result.jpg', haystack_img)
+
+    return points
+
+
+points = findClickPositions('albion_cabbage.jpg', 'albion_farm.jpg', debug_mode='points')
+print(points)
+points = findClickPositions('albion_turnip.jpg', 'albion_farm.jpg', 
+                            threshold=0.70, debug_mode='rectangles')
+print(points)
+print('Done.')
